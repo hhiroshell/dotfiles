6a3be347d33ec2a32264a2355b5574e870ef8f29
hhiroshell <hhiroshell@gmail.com>
2023-03-06 00:34:56 +0900

    move the .config directory to under dotfiles repository

---
+++ .config/nvim/init.lua
@@ -0,0 +1,113 @@
+-- ===============
+-- setting options
+-- ===============
+
+local o = vim.o
+
+--- base of gui colors
+o.termguicolors = true
+o.background = "light"
+
+--- appearance
+o.number = true
+
+--- case insensitive search
+o.ignorecase = true
+
+--- use 4 spaces instead of a Tab
+o.expandtab = true
+o.tabstop = 4
+o.shiftwidth = 4
+
+-- =======
+-- autocmd
+-- =======
+
+--- augroup
+vim.api.nvim_create_augroup('my-augroup', {})
+
+--- remove all trailing whitespace automatically
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+  group = 'my-augroup',
+  pattern = {"*"},
+  command = ":%s/\\s\\+$//e",
+})
+
+--- set shell environment for calling system commands
+o.shell = '/usr/bin/zsh --login'
+
+--- disable windows IME when exiting from insert mode.
+--- zenhan.exe must be installed on PATH.
+vim.api.nvim_create_autocmd({"InsertLeave", "CmdlineLeave"}, {
+  group = 'my-augroup',
+  pattern = {"*"},
+  command = ":call system('zenhan.exe 0')",
+})
+
+--- send yanked text to the windows clipboard.
+vim.api.nvim_create_autocmd({"TextYankPost"}, {
+  group = 'my-augroup',
+  pattern = {"*"},
+  command = ":call system('clip.exe', @\")",
+})
+
+-- ==========
+-- key remaps
+-- ==========
+
+local map = vim.api.nvim_set_keymap
+
+--- map the leader key
+map('n', '<Space>', '', {})
+vim.g.mapleader = ' '
+
+-- ===============
+-- plugin settings
+-- ===============
+
+require('plugins')
+
+-- zenbones (color theme)
+require('zenbones')
+
+vim.api.nvim_create_autocmd({"ColorScheme"}, {
+  group = 'my-augroup',
+  pattern = {"zenbones"},
+  command = "lua require \"customize_zenbones\"",
+})
+
+vim.cmd.colorscheme "zenbones"
+
+require('lualine').setup {
+  options = { theme = 'zenbones' },
+  sections = {
+    lualine_c = {
+      { 'filename', path = 1 },
+    },
+  }
+}
+
+-- telescope (fuzzy finder)
+require('telescope').setup({
+  defaults = {
+    mappings = {
+      i = {
+        ['<esc>'] = require('telescope.actions').close,
+      },
+    },
+  },
+})
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require('nvim-web-devicons').setup {
+  color_icons = false,
+}
+
+-- comment
+require('Comment').setup()
+
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,57 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use {
+    'wbthomason/packer.nvim',
+  }
+
+  use {
+    'mcchrish/zenbones.nvim',
+    requires = {
+      { 'rktjmp/lush.nvim' },
+    },
+  }
+
+  use {
+    'nvim-tree/nvim-web-devicons',
+  }
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = {
+      { 'nvim-tree/nvim-web-devicons', opt = true },
+    }
+  }
+
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    requires = {
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-tree/nvim-web-devicons', opt = true },
+    },
+  }
+
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
+
+  -- Automatically set up this configuration after cloning packer.nvim
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
