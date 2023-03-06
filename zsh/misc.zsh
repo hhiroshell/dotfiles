ba195031b9b3eeed20ba50567d3cad262382fbd0
hhiroshell <hhiroshell@gmail.com>
2023-03-06 21:32:59 +0900

    disbale wsl utilities in other environments

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -23,29 +23,31 @@ alias vim='nvim'
 # Utilities
 # ==========
 
-# cf. https://zenn.dev/kaityo256/articles/open_command_on_wsl
-function open() {
-    if [ $# != 1 ]; then
-        explorer.exe .
-    else
-        if [ -e $1 ]; then
-            cmd.exe /c start "$(wslpath -w $1)" 2> /dev/null
-	elif [[ "$1" =~ "^http://" ]] || [[ "$1" =~ "^https://" ]] ; then
-            cmd.exe /c start "$1" 2> /dev/null
+# WSL utilities for compatibility with masOS X
+if [[ "$(uname -r)" == *microsoft* ]]; then
+    # cf. https://zenn.dev/kaityo256/articles/open_command_on_wsl
+    function open() {
+        if [ $# != 1 ]; then
+            explorer.exe .
         else
-            echo "open: $1 : No such file or directory"
+            if [ -e $1 ]; then
+                cmd.exe /c start "$(wslpath -w $1)" 2> /dev/null
+        elif [[ "$1" =~ "^http://" ]] || [[ "$1" =~ "^https://" ]] ; then
+                cmd.exe /c start "$1" 2> /dev/null
+            else
+                echo "open: $1 : No such file or directory"
+            fi
         fi
-    fi
-}
+    }
 
-# cf. https://zenn.dev/kondounagi/scraps/184c884b5804a4
-alias pbcopy='clip.exe'
-alias pbpaste='powershell.exe -Command Get-Clipboard'
+    # cf. https://zenn.dev/kondounagi/scraps/184c884b5804a4
+    alias pbcopy='clip.exe'
+    alias pbpaste='powershell.exe -Command Get-Clipboard'
+fi
 
 # today
 today() {
     date '+%Y-%m-%d'
-
 }
 
 # history fuzzy finder
