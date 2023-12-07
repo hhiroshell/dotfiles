49df39468c9fb8156874f9354df95d8f95f5bf31
hhiroshell <hhiroshell@gmail.com>
2023-12-07 19:32:53 +0900

    nodenv

--- zsh/sdk.zsh
+++ zsh/sdk.zsh
@@ -1,3 +1,9 @@
+# sdkman
 export SDKMAN_DIR="$HOME/.sdkman"
 
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+# nodenv
+export PATH="$PATH:$HOME/.nodenv/bin"
+
+eval "$(nodenv init - zsh)"
