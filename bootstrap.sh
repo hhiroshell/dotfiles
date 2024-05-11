8990ed8bf9e6b37a3767b9e8605733c9dfb8aa8d
Hiroshi Hayakawa <hhiroshell@gmail.com>
2024-05-11 16:01:32 +0900

    add a bootstrapping script

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+base_path="/home/hhiroshell/src/github.com/hhiroshell/dotfiles/home"
+declare -a targets=(
+    ".config"
+    ".gitconfig"
+    ".gitignore_global"
+    ".ssh/config"
+    ".zimrc"
+    ".zshrc"
+)
+
+for target in "${targets[@]}"; do
+    src="${base_path}/${target}"
+    dest="$HOME/${target}"
+
+    dest_dir=$(dirname "${dest}")
+    if [ ! -d "${dest_dir}" ]; then
+        mkdir -p "${dest_dir}"
+    fi
+
+#     ln -s "${src}" "${dest}"
+    echo "ln -s ${src} ${dest}"
+    echo "Created symlink for ${target}"
+done
