2dde29c6359f82076e48988dcde8ed2bb17f4839
Hiroshi Hayakawa <hhiroshell@gmail.com>
2024-05-12 10:33:49 +0900

    stop pinning the version of nvim.telescope

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -35,7 +35,7 @@ return require('packer').startup(function(use)
   }
 
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    'nvim-telescope/telescope.nvim',
     requires = {
       { 'nvim-lua/plenary.nvim' },
       { 'nvim-tree/nvim-web-devicons', opt = true },
