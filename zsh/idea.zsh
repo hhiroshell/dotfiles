13ac11c7e6aada968c5c5fcaa4f3fc588ce5b7a7
Hiroshi Hayakawa <hhiroshell@gmail.com>
2024-06-05 18:08:55 +0900

    add installation utilities for the intellij idea

--- zsh/idea.zsh
+++ zsh/idea.zsh
@@ -29,3 +29,78 @@ _ghq-idea() {
 }
 
 alias idea='_ghq-idea'
+
+# ======================
+# Installation utilities
+# ======================
+
+list-idea-archives() {
+    local current=$(cat /usr/local/idea-IU/product-info.json | jq -r '.version')
+
+    local candidates=$(curl -s "https://data.services.jetbrains.com/products?code=IIU&release.type=release" \
+        | jq -r '.[].releases[].downloads.linux.link' \
+        | grep -v "null" \
+        | head \
+        | while read url; do basename "$url"; done
+    )
+
+    echo ${candidates} | while read archive; do
+        if [[ "${archive}" =~ ${current} ]]; then
+            echo "${archive} (<- installed)"
+        else
+            echo "${archive}"
+        fi
+    done
+}
+
+install-idea() {
+    local archive
+    archive=$(list-idea-archives | fzf)
+    if [ $? -ne 0 ]; then
+        return 1
+    fi
+
+    if [[ "${archive}" =~ "installed" ]]; then
+        echo "You are already using the specified version."
+        return 1
+    fi
+
+    tmpdir="$(mktemp -d)"
+
+    echo "Downloading ${archive} ..."
+    url=$(curl -s "https://data.services.jetbrains.com/products?code=IIU&release.type=release" \
+        | jq -r '.[].releases[].downloads.linux.link' \
+        | grep -e "${archive}" \
+    )
+    curl -L "${url}" --output "${tmpdir}/${archive}"
+
+
+    echo "Extracting ${archive} ..."
+    tar -x -f "${tmpdir}/${archive}" -C ${tmpdir}
+    base="$(tar -t -f ${tmpdir}/${archive} | grep '/$' | head -n 1 | sed 's/\/$//')"
+    mv "${tmpdir}/${base}" "${tmpdir}/idea-IU"
+
+    echo "Removing old installation..."
+    sudo rm -rf /usr/local/idea-IU
+    if [ $? -ne 0 ]; then
+        echo "failed to remove old installation"
+        return 1
+    fi
+
+    echo "Installing..."
+    sudo mv "${tmpdir}/idea-IU" /usr/local
+    if [ $? -ne 0 ]; then
+        echo "failed to extract the idea archive"
+        return 1
+    fi
+
+    echo "Done."
+}
+
+# =====
+# PATH
+# =====
+
+# "$HOME/go/bin" is for binaries installed via "go install"
+export PATH="$PATH:/usr/local/idea-IU/bin"
+
