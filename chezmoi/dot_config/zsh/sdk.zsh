a16140239acb669472efe4770fc2e5cac14507f1
Hiroshi Hayakawa <hhiroshell@gmail.com>
2026-03-08 23:53:21 +0900

    Add nvm and pnpm shell setup to sdk.zsh

--- chezmoi/dot_config/zsh/sdk.zsh
+++ chezmoi/dot_config/zsh/sdk.zsh
@@ -9,6 +9,26 @@ fi
 
 
 # =====
+# Node (nvm)
+# =====
+
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+
+# =====
+# pnpm
+# =====
+
+export PNPM_HOME="$HOME/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+
+
+# =====
 # Rust
 # =====
 
